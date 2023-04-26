eeadfe8ab85957aa4ec8cdf8aff4ab41a76dfe56
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-04-26 11:18:49 +0900

    Barを動かす

---
+++ Sample/BallApp/Bar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BallApp {
+    class Bar : Obj {
+
+        public Bar(double xp, double yp)
+           : base(350, 400, "pic\\bar.png") {
+            base.MoveX = 10;
+            base.MoveY = 0;
+
+        }
+
+        public void Move(Keys direction) {
+            if (direction ==　Keys.Right)
+            {
+                if (PosX < 800)
+                {
+                    PosX += MoveX;
+                }
+            }else if(direction == Keys.Left)
+            {
+                if (PosX > 0)
+                {
+                    PosX -= MoveX;
+                }
+            }
+
+        }
+        public override void Move() {
+
+        }
+    }
+}
--- Sample/BallApp/Program.cs
+++ Sample/BallApp/Program.cs
@@ -12,6 +12,10 @@ namespace BallApp {
         private Obj obj;
         private PictureBox pb;
 
+        Bar bar;
+        PictureBox pbBar;
+
+        //Listコレクション
         private List<Obj> balls = new List<Obj>(); //ボール格納
         private List<PictureBox> pbs = new List<PictureBox>(); //表示用
 
@@ -21,34 +25,33 @@ namespace BallApp {
         }
 
         public Program() {
-            //this.Width = 1200; //幅プロパティ
-            //this.Height = 800; // 高さプロパティ
-            this.Size = new Size(800, 600);
 
+            //フォーム
+            this.Size = new Size(800, 600);
             this.BackColor = Color.Green;
             this.Text = "BallGame";
 
             this.MouseClick += Program_MouseClick;
             this.KeyDown += Program_KeyDown;
 
+            //Barインスタンス生成
+            bar = new Bar(340 , 500);
+            pbBar = new PictureBox();
+            pbBar.Image = bar.Image;
+            pbBar.Location = new Point((int)bar.PosX,(int)bar.PosY);
+            pbBar.Size = new Size(150, 10);
+            pbBar.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbBar.Parent = this;
+
             moveTimer = new Timer();
             moveTimer.Interval = 1; //タイマーのインターバル
 
             moveTimer.Tick += MoveTimer_Tick;　//デリゲード登録
         }
-
-        //キーが押された時のイベントハンドラ
-        private void Program_KeyDown(object sender, KeyEventArgs e) {
-            if (e.KeyCode == Keys.A)
-            {
-
-            }
-
-        }
-
+        
         //マウスクリック時のイベントハンドラ
         private void Program_MouseClick(object sender, MouseEventArgs e) {
-
+            
             if (e.Button == MouseButtons.Left)
             {
                 //サッカーボールインスタンス生成
@@ -72,6 +75,16 @@ namespace BallApp {
             pbs.Add(pb);
             moveTimer.Start(); //タイマースタート
         }
+
+
+
+        //キーが押された時のイベントハンドラ
+        private void Program_KeyDown(object sender, KeyEventArgs e) {
+                bar.Move(e.KeyData);
+                pbBar.Location = new Point((int)bar.PosX, (int)bar.PosY);
+        }
+
+
         //タイマータイムアウト用のイベントハンドラ
         private void MoveTimer_Tick(object sender, EventArgs e) {
             for (int i = 0; i < balls.Count; i++)
