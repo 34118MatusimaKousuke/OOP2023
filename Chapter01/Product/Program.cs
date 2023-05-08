7ca167b96ecdcbf10b1137146d3cce53e2fe17a6
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-05-08 11:38:41 +0900

    0508演習1

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -10,12 +10,29 @@ namespace ProductSample {
     class Program {
         static void Main(string[] args) {
 
+            #region P26のサンプルプログラム
             //インスタンス生成
-            Product karinto = new Product(123, "かりんとう", 180);
-            Product daihuku = new Product(124, "大福", 280);
-            Console.WriteLine(karinto.GetPriceIncludingTax());
-            Console.WriteLine(daihuku.GetPriceIncludingTax());
+            //Product karinto = new Product(123, "かりんとう", 180);
+            //Product daihuku = new Product(124, "大福", 280);
 
+            //Console.WriteLine(karinto.GetPriceIncludingTax());
+            //Console.WriteLine(daihuku.GetPriceIncludingTax());
+            #endregion
+
+            //DateTime date = new DateTime(2023,5,8);
+            DateTime date = DateTime.Today;
+
+            
+            //今日の日付
+            Console.WriteLine("今日の日付:" + date );
+
+            //10日後を求める
+            DateTime daysAfter10 = date.AddDays(10);
+            Console.WriteLine("十日後:" + daysAfter10);
+
+            //10日前を求める
+            DateTime daysbefore10 = date.AddDays(-10);
+            Console.WriteLine("十日前:" + daysbefore10);
         }
     }
 }
