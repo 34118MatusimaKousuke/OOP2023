99c362a41edea3d95e32ca4d60dc945d0cb01c5f
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-05-08 10:54:07 +0900

    p26まで

---
+++ Chapter01/Product/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+
+    //商品クラス
+    class Program {
+        static void Main(string[] args) {
+
+            //インスタンス生成
+            Product karinto = new Product(123, "かりんとう", 180);
+            Product daihuku = new Product(124, "大福", 280);
+            Console.WriteLine(karinto.GetPriceIncludingTax());
+            Console.WriteLine(daihuku.GetPriceIncludingTax());
+
+        }
+    }
+}
