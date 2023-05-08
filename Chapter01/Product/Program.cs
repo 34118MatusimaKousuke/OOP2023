0fe1baea4991b9d35c34f6b56ba2575d225ab8e6
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-05-08 11:49:45 +0900

    0508演習1

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -24,15 +24,16 @@ namespace ProductSample {
 
             
             //今日の日付
-            Console.WriteLine("今日の日付:" + date );
+            Console.WriteLine("今日の日付:" + date.Year + "年" + date.Month + "月" + date.Day + "日" );
 
             //10日後を求める
             DateTime daysAfter10 = date.AddDays(10);
-            Console.WriteLine("十日後:" + daysAfter10);
+            Console.WriteLine("十日後:" + daysAfter10.Year + "年" + daysAfter10.Month + "月" + daysAfter10.Day + "日");
 
             //10日前を求める
             DateTime daysbefore10 = date.AddDays(-10);
-            Console.WriteLine("十日前:" + daysbefore10);
+            Console.WriteLine("十日前:" + daysbefore10.Year + "年" + daysbefore10.Month + "月" + daysbefore10.Day + "日");
+
         }
     }
 }
