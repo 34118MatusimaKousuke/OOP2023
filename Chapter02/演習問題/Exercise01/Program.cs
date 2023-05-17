1d467734c75c6f0b0b1cf480cdeb8e2322d2ee5a
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-05-17 11:03:39 +0900

    2-1-2

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -7,6 +7,23 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     class Program {
         static void Main(string[] args) {
+            var songs = new Song[] {
+                new Song("Let it be", "The Beatles", 243),
+                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+                new Song("Close To You", "Carpenters", 276),
+                new Song("Honesty", "Billy Joel", 231),
+                new Song("I Will Always Love You", "Whitney Houston", 273),
+            };
+            Console.WriteLine(songs);
+
+            var lists = new List<Song>();
+            lists.Add(new Song("Let it be", "The Beatles", 243));
+            lists.Add(new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293));
+            lists.Add(new Song("Close To You", "Carpenters", 276));
+            lists.Add(new Song("Honesty", "Billy Joel", 231));
+            lists.Add(new Song("I Will Always Love You", "Whitney Houston", 273));
+
+            Console.WriteLine(lists[1]);
         }
     }
 }
---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Song {
+
+        //歌のタイトル
+        public string Title { get; set; }
+
+        //アーティスト名
+        public string ArtistName { get; set; }
+
+        //演奏時間
+        public int Length { get; set; }
+
+        public Song(string title, string artistName, int Length) {
+            Title = title;
+            ArtistName = artistName;
+            Length = Length;
+    }
+
+}
