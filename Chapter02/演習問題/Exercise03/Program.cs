e9044fea2bfbd4d73b6dbe629ed3cb44232d82ca
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-05-22 09:54:21 +0900

    P79演習問題2-3 完成

--- Chapter02/演習問題/Exercise03/Program.cs
+++ Chapter02/演習問題/Exercise03/Program.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
+            var sales = new SalesCounter(@"data\sales.csv");
+            var amountPetStore = sales.GetPerStoreSales();
+            foreach (var obj in amountPetStore) {
+                Console.WriteLine(String.Format("{0}{1:#,0}", obj.Key, obj.Value));
+            }
         }
     }
 }
