ab31b83e6bcd4da09c5dc3b7606104aa46dcd014
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-05-23 11:12:09 +0900

    P90まで

--- Chapter03/Section03/Program.cs
+++ Chapter03/Section03/Program.cs
@@ -10,6 +10,13 @@ namespace Section03 {
             var list = new List<string> {
                "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
             };
+
+            //var exists = list.Exists(s => s[0] == 'L');
+            //Console.WriteLine(exists);
+
+            var names = list.ConvertAll(s => s.ToLower());
+            names.ForEach(s => Console.WriteLine(s));
+
         }
     }
 }
