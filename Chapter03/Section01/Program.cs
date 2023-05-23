b74a578577041acbfb440afbd7d004d3c58d98cd
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-05-23 10:32:48 +0900

    P87まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -8,10 +8,10 @@ namespace Section01 {
     class Program {
 
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4, 325, 15, 46, 98, 56};
 
-            int count = numbers.Count(n => n % 2 == 0);
-            Console.WriteLine(count);
+            var sum = numbers.Where(n => n % 2 ==0).Sum();
+            Console.WriteLine(sum);
         }
     }
 }
