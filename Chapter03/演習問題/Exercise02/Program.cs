5dd053ec568e547d599ae67ffa774358b60237f1
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-05-24 12:02:49 +0900

    演習問題3-2完成

---
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var names = new List<string> {
+                 "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+            Console.WriteLine("***** 3.1 *****");
+            Exercise2_1(names);
+            Console.WriteLine("***** 3.2 *****");
+            Exercise2_2(names);
+            Console.WriteLine("***** 3.3 *****");
+            Exercise2_3(names);
+            Console.WriteLine("***** 3.4 *****");
+            Exercise2_4(names);
+        }
+
+        private static void Exercise2_1(List<string> names) {
+            Console.WriteLine("都市名を入力。空行で終了");
+            do {
+                var line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line)) {
+                    break;
+                }
+                var find = names.FindIndex(s => s == line);
+
+                Console.WriteLine(find);
+            } while (true);
+        }
+
+        private static void Exercise2_2(List<string> names) {
+            var count = names.Where(s => s.Contains('o')).Count();
+            Console.WriteLine(count);
+        }
+
+        private static void Exercise2_3(List<string> names) {
+            var ocity = names.Where(s => s.Contains('o')).ToList();
+            foreach (var s in ocity) {
+                Console.WriteLine(s);
+            }
+        }
+        private static void Exercise2_4(List<string> names) {
+            var list = names.Where(s => s[0] == 'B').Select(s => s.Length);
+            foreach (var s in list) {
+                Console.WriteLine(s);
+
+            }
+        }
+    }
+}
