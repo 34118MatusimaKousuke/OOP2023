2e09ceb4a0fdcbd6d58771e4bff20b0a92fffc74
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-05-31 11:00:48 +0900

    P134 演習問題4=2完成

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -8,13 +8,56 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
+            // 4.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2020, 12),
+            };
+            // 4.2.2
+            Console.WriteLine("\n- 4.2.2 ---");
+            Exercise2_2(ymCollection);
+            Console.WriteLine("\n- 4.2.4 ---");
 
-            var ym = new YearMonth(2023,12);
-            var c21 = ym.Is21Century;
-            var ymNextMonth = ym.AddOneMonth();
+            // 4.2.4
+            Exercise2_4(ymCollection);
+            Console.WriteLine("\n- 4.2.5 ---");
 
+
+            // 4.2.5
+            Exercise2_5(ymCollection);
+        }
+
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+            foreach (var n in ymCollection) {
+                Console.WriteLine(n);
+            }
+        }
+
+        static YearMonth FindFirst21C(YearMonth[] yms) {
+            foreach (var n in yms) {
+                if (n.Is21Century) {
+                   return n;
+                }
+            }
+            return null;
+        }
+
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+            var ym = FindFirst21C(ymCollection);
+            if (ym == null) {
+                Console.WriteLine("21世紀のデータはありません");
+            }
             Console.WriteLine(ym);
-            Console.WriteLine(ymNextMonth);
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection) {
+            var ym = ymCollection.Select(n => n.AddOneMonth()).ToArray();
+            foreach (var n in ym) {
+                Console.WriteLine(n);
+            }
         }
     }
 }
