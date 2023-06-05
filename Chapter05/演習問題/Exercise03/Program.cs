dd63ab9223c3da27156a4f0cde825ae515b39f1d
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-06-05 10:22:13 +0900

    P159演習問題5-3完成

--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -26,19 +26,36 @@ namespace Exercise03 {
 
         private static void Exercise3_1(string text) {
             var count = text.Count(s => s == ' ');
-            Console.WriteLine(count);
+            Console.WriteLine("空白数：" + count);
         }
 
         private static void Exercise3_2(string text) {
+            var replaced = text.Replace("big","small");
+            Console.WriteLine(replaced);
         }
 
         private static void Exercise3_3(string text) {
+            var count = text.Split(' ').Count();
+            Console.WriteLine("単語数："+ count);
         }
 
         private static void Exercise3_4(string text) {
+            var count = text.Split(' ');
+            foreach (var s in count) {
+                if (s.Length <= 4) {
+                    Console.WriteLine(s);
+                }
+            }
         }
 
         private static void Exercise3_5(string text) {
+            var word = text.Split(' ').ToList();
+            var sb = new StringBuilder();
+            foreach (var s in word) {
+                sb.Append(s + ' ');
+            }
+            var t1 = sb.ToString();
+            Console.WriteLine(t1);
         }
     }
 }
