fd0c8f248d95345fea83d70159fbef64d1e1fed8
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-06-07 09:49:55 +0900

    タイトルの長い順に出力

--- Chapter06/Section01/Program.cs
+++ Chapter06/Section01/Program.cs
@@ -10,12 +10,20 @@ namespace Section01 {
             var numbers = new List<int> { 9, 7, 5, 4, 2, 5, 4, 0, 4, 1, 0, 4 };
             Console.WriteLine(numbers.Average());
 
-
+            #region タイトルに物語を含む本の平均ページ数出力
             var books = Books.GetBooks();
-            var price = books.Where(s => s.Title.Contains("物語")).Average(s => s.Pages);
+            var page = books.Where(s => s.Title.Contains("物語")).Average(s => s.Pages);
 
-            Console.WriteLine(price + "ページ");
+            Console.WriteLine(page + "ページ");
+            #endregion
 
+            #region タイトルの長い順に出力
+            var title = books.OrderByDescending(s => s.Title.Length);
+            foreach (var s in title) {
+                Console.WriteLine(s.Title);
+
+            }
+            #endregion
         }
     }
 }
