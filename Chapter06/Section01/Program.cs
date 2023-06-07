d6ffb36fd03074dfd26ee814e09e32792fa48d66
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-06-07 09:40:28 +0900

    タイトルに物語を含む本の平均ページ数を出力

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 9, 7, 5, 4, 2, 5, 4, 0, 4, 1, 0, 4 };
+            Console.WriteLine(numbers.Average());
+
+
+            var books = Books.GetBooks();
+            var price = books.Where(s => s.Title.Contains("物語")).Average(s => s.Pages);
+
+            Console.WriteLine(price + "ページ");
+
+        }
+    }
+}
