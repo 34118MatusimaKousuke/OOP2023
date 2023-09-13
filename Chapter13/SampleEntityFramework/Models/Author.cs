fcca751d997fff90fbf7aa258956dfe84e2c2f2f
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-09-13 11:09:34 +0900

    データー注釈

--- Chapter13/SampleEntityFramework/Models/Author.cs
+++ Chapter13/SampleEntityFramework/Models/Author.cs
@@ -1,6 +1,7 @@
 ﻿using SampleEntityFramework.Modeks;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ using System.Threading.Tasks;
 namespace SampleEntityFramework.Models {
     public class Author {
         public int Id { get; set; } //主キー
+        [MaxLength(30)]
+        [Required]
         public string Name { get; set; }
         public DateTime Birthday { get; set; }
         public string Gender { get; set; }
--- Chapter13/SampleEntityFramework/Models/Book.cs
+++ Chapter13/SampleEntityFramework/Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using SampleEntityFramework.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 namespace SampleEntityFramework.Modeks {
     public class Book {
         public int Id { get; set; } //主キー
+        [Required]
         public string Title { get; set; }
         public int PublishedYear { get; set; }
         public virtual Author Author { get; set; }
