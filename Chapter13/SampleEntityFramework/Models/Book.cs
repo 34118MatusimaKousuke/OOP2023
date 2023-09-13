f5c5a8609119197e90fa5320ddfe0a86550b55e6
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-09-13 11:02:22 +0900

    P339完成

---
+++ Chapter13/SampleEntityFramework/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using SampleEntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEntityFramework.Modeks {
+    public class Book {
+        public int Id { get; set; } //主キー
+        public string Title { get; set; }
+        public int PublishedYear { get; set; }
+        public virtual Author Author { get; set; }
+    }
+}
