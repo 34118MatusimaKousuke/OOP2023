90616730c663567f1622b8b0926a4a4a142c62cb
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-10-03 10:27:30 +0900

    選択したタイトルのRSS表示

--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -4,14 +4,39 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace RssReader {
     public partial class Form1 : Form {
+        List<ItemData> ItemDatas = new List<ItemData>();
         public Form1() {
             InitializeComponent();
+        }
+
+        private void btGet_Click(object sender, EventArgs e) {
+            using (var wc = new WebClient()) {
+                var url = wc.OpenRead(tbUrl.Text);
+                XDocument xdoc = XDocument.Load(url);
+
+                var nodes = xdoc.Root.Descendants("item").Select(s => new ItemData {
+                    Title = (string)s.Element("title"),
+                    Link = (string)s.Element("link")
+                });
+
+                foreach (var s in nodes) {
+                    lbRssTitle.Items.Add(s.Title);
+                    ItemDatas.Add(s);
+                }
+            }
+        }
+
+        private void lbRssTitle_SelectedIndexChanged(object sender, EventArgs e) {
+            var num = lbRssTitle.SelectedIndex;
+            wbBrowser.Navigate(ItemDatas[num].Link);
         }
     }
 }
