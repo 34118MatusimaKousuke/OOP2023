4301155e8bc8ea754a7ddfc27f089c9872bbafcb
松島 光佑 <OIBC34118@ojs.ac.jp>
2023-10-17 10:21:18 +0900

    Chapter17 10月21日

---
+++ Chaptre17/Section01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            var greetings = new List<GreetingBase>() {
+                new GreetingMorning(),
+                new GreetingAfternoon(),
+                new GreetingEvening(),
+            };
+
+            foreach (var s in greetings) {
+                string msg = s.GetMessage();
+                Console.WriteLine(msg);
+            }
+
+        }
+    }
+}
